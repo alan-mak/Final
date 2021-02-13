37cc76d8022c8d19ea29c2ebf3a84e444aabe0b1
justinkwanchan <justinkwanchan@gmail.com>
2021-02-13 12:27:40 -0500

    Add Nav component

--- front-end/src/stories/index.stories.js
+++ front-end/src/stories/index.stories.js
@@ -3,19 +3,23 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
 import Button from '../components/Button';
+import Nav from '../components/Nav';
 
 // export default {
 //   title: 'Button',
 //   component: Button,
 // };
 
-
-
-storiesOf("Button", module)
+storiesOf('Button', module)
   // .addParameters({
   //   backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
-  .add("Login", () => <Button message="Login?"/>)
-  .add("Signup", () => <Button message="signup" />)
-  .add("I need help!", () => <Button message="I need Help!" />)
-  .add("I can help!", () => <Button message="I can help!" />)
- 
+  .add('Login', () => <Button message='Login?' />)
+  .add('Signup', () => <Button message='signup' />)
+  .add('I need help!', () => <Button message='I need Help!' />)
+  .add('I can help!', () => <Button message='I can help!' />);
+
+storiesOf('Nav', module)
+  // .addParameters({
+  //   backgrounds: [{ name: 'dark', value: '#222f3e', default: true }],
+  // })
+  .add('Nav', () => <Nav />);
