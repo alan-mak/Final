47cd4280776fe9092bd50f715406f5c0f6726666
CD-Mackay <connor.mackay@gmail.com>
2021-02-13 11:16:44 -0500

    setup storybook test suite

--- front-end/src/stories/Button.stories.js
+++ front-end/src/stories/Button.stories.js
@@ -1,36 +1,36 @@
 import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 
-import { Button } from './Button';
+import Button from '../components/Button';
 
-export default {
-  title: 'Example/Button',
-  component: Button,
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
-};
+// export default {
+//   title: 'Button',
+//   component: Button,
+// };
 
-const Template = (args) => <Button {...args} />;
 
-export const Primary = Template.bind({});
-Primary.args = {
-  primary: true,
-  label: 'Button',
-};
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
-};
+storiesOf("Button", module)
+  // .addParameters({
+  //   backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  .add("Login", () => <Button message="Login?"/>)
+  .add("Signup", () => <Button message="signup" />)
+ 
 
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
 
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
-};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
