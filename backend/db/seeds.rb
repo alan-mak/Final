ea61613e887312fa0f65f712df941ebbd7cfe8ec
justinkwanchan <justinkwanchan@gmail.com>
2021-02-14 23:27:08 +0000

    Add faker and seed Users table

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Faker::Config.locale = "en-CA"
+
+puts "Seeding Data ..."
+
+## USERS
+
+puts "Re-creating Users ..."
+
+User.destroy_all
+
+100.times do
+  User.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.free_email,
+    password: "test",
+    street: "#{Faker::Address.building_number} #{Faker::Address.street_name} #{Faker::Address.street_suffix}",
+    city: Faker::Address.city,
+    province: Faker::Address.state_abbr,
+    post_code: Faker::Address.postcode
+  )
+end
+
+puts "DONE!"
