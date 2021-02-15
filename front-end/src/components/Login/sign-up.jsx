c3f79c480e921316c8f1418e84b2060408c4547e
Alan Mak <alanmak95@gmail.com>
2021-02-15 14:56:50 -0500

    Set up hook

--- front-end/src/components/Login/sign-up.jsx
+++ front-end/src/components/Login/sign-up.jsx
@@ -1,47 +1,96 @@
-import React, {Component} from "react";
+import React, { useState } from "react";
 
 import Button from "../Button";
 import "./sign-up.scss"
 
 export default function SignUp(props) {
 
+  const [input, setInput] = useState({
+    email:"",
+    password:"",
+    confPassword:"",
+    street:"",
+    city:"",
+    province:"",
+    postCode:"",
+  });
+  const [error, setError] = useState("");
+
+  const handleInput = event => setInput({
+    ...input,
+    [event.currentTarget.name]: event.currentTarget.value
+  });
+
+  function validate() {
+    if (!input.email || !input.password || !input.street || !input.city || !input.province || !input.postCode) {
+      setError("ERROR: Cannot be blank")
+      return
+    }
+  };
+
   return (
     <div className="base-container" >
       <h1>Sign Up</h1>
       <div className="content">
-        <form autoComplete="off" onSubmit={event =>event.preventDefault()}>
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
+          <section>{error}</section>
           <div className="form-group">
             <label>EMAIL: </label>
             <input
             type="email"
             placeholder="MScott@dunder.com"
-            name="email" />
+            name="email"
+            value={input.email}
+            onChange={handleInput}
+            />
           </div>
           <div className="form-group">
             <label>PASSWORD: </label>
-            <input type="password" placeholder="TypeYour Password" name="password" />
-          </div>
-          <div className="form-group">
-            <label>CONFIRM PASSWORD: </label>
-            <input type="password" placeholder="Re-EnterYour Password" name="confPassword" />
+            <input
+            type="password"
+            placeholder="Type Your Password"
+            name="password"
+            value={input.password}
+            onChange={handleInput}
+            />
           </div>
           <div className="form-group">
             <label>STREET: </label>
-            <input type="text" placeholder="123 Street"name="street" />
+            <input
+            type="text"
+            placeholder="123 Street"
+            name="street"
+            value={input.street}
+            onChange={handleInput}
+            />
           </div>
           <div className="form-group">
             <label>CITY: </label>
-            <input type="text" placeholder="Toronto"name="city" />
+            <input
+            type="text"
+            placeholder="Toronto"
+            name="city"
+            value={input.city}
+            onChange={handleInput}/>
           </div>
           <div className="form-group">
             <label>PROVINCE: </label>
-            <input type="text" placeholder="Ontario"name="province" />
+            <input type="text"
+            placeholder="Ontario"
+            name="province"
+            value={input.province}
+            onChange={handleInput}/>
           </div>
           <div className="form-group">
             <label>POSTAL CODE: </label>
-            <input type="text" placeholder="A1B2C3"name="post_code" />
+            <input
+            type="text"
+            placeholder="A1B2C3"
+            name="postCode"
+            value={input.postCode}
+            onChange={handleInput}/>
           </div>
-          <Button type="submit" message="Sign Up"/>
+          <Button type="submit" message="Sign Up" onClick={validate}/>
         </form>
       </div>
     </div>
