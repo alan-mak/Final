163f16537ecf48615a41c5981b100a83e171f6c0
Alan Mak <alanmak95@gmail.com>
2021-02-16 12:42:36 -0500

    Add JWT to application controller

--- backend/app/controllers/api/users_controller.rb
+++ backend/app/controllers/api/users_controller.rb
@@ -1,13 +1,13 @@
 class Api::UsersController < ApplicationController
   def index
-    users = User.all
-    render json: users
+    @users = User.all
+    render json: @users
   end
 
   def create
-    users = User.create(user_params)
-    if users
-      render json: { user: UserSerializer.new(users) }, status: :created
+    @users = User.create(user_params)
+    if @users
+      render json: { user: UserSerializer.new(@users) }, status: :created
     else
       render json: { error: 'failed to create user'}, status: not_accepted
     end
