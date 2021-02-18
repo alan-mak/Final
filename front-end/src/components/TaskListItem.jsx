7b6c2ddae171539a1309f781d94385c5f3d9e520
justinkwanchan <justinkwanchan@gmail.com>
2021-02-18 16:20:23 -0500

    Fix scrolling on Task List page - add styling for accepted message

--- front-end/src/components/TaskListItem.jsx
+++ front-end/src/components/TaskListItem.jsx
@@ -1,24 +1,34 @@
 import React, { useState } from 'react';
 import Button from '../components/Button';
-import Maps from './Maps'
+import Maps from './Maps';
 import '../components/tasks.scss';
 
 export default function TaskListItem(props) {
-    const [accepted, setAccepted] = useState(false);
+  const [accepted, setAccepted] = useState(false);
   const takeTask = function () {
     setAccepted(true);
     props.onTake(props.setter, 1);
-  }
+  };
   return (
     <div className='task-item'>
       <h2 className='task-list-item-title'>{props.name}</h2>
       <div className='task-list-item-title-underline'></div>
       <p className='task-list-item-description'>{props.description}</p>
-      <Maps />
-      <div className='task-list-item-buttons'>    
-        {accepted && <h2>You accepted this task! Click Clarify to contact the poster!</h2>}
-        {!accepted &&  <Button message="Accept!" onClick={takeTask}/> }
-        <Button message="Clarify!" onClick={() => props.onAccept(props.setter, props.name)}/> 
+      <div className='task-list-item-distance'>
+        <Maps />
+      </div>
+      {accepted && (
+        <p className='task-list-item-accepted-message'>
+          You accepted this task! Click <strong>Clarify</strong> to contact the
+          poster!
+        </p>
+      )}
+      <div className='task-list-item-buttons'>
+        {!accepted && <Button message='Accept!' onClick={takeTask} />}
+        <Button
+          message='Clarify!'
+          onClick={() => props.onAccept(props.setter, props.name)}
+        />
       </div>
     </div>
   );
