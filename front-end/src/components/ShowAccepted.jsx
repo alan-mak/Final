15b92e1fe1d09d7f78843289098ff76a99b411ec
CD-Mackay <connor.mackay@gmail.com>
2021-02-18 17:35:35 -0500

    implement dynamic rendering of accepted tasks

--- front-end/src/components/ShowAccepted.jsx
+++ front-end/src/components/ShowAccepted.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react';
 
 export default function ShowAccepted (props) {
-console.log("props.tasks: ", JSON.stringify(props.tasks));
-console.log('props: ', props)
- let taskList = props.tasks.map(item => {
-  <li key={item.id} id={item.id}>{item.name}</li>});
-  console.log("parsedprops ", taskList);
+
+  let taskList = props.tasks.map(item => {
+  return <li key={item.id} id={item.id}>{item.name}</li>});
+  
   return (
     <div id="task-div">
+      <p>Thank's for helping out, here's what you promised to do!</p>
       <ul>
       { taskList }
       </ul>
