7ed273e4bff988558b57b8babc48991cfc4ba80b
Alan Mak <alanmak95@gmail.com>
2021-02-19 14:11:45 -0500

    Slightly better

--- front-end/src/components/Maps/index.jsx
+++ front-end/src/components/Maps/index.jsx
@@ -12,12 +12,6 @@ export default function Maps(props) {
   const token = sessionStorage.getItem('token')
   const userID = jwt_decode(token)
 
-  const showAddress = (searchID) => {
-    const turtle = props.userList.find(user => user.id === searchID)
-    if (turtle) {
-      return (turtle.street.split(" ").join("+") +","+ turtle.city.split(" ").join("+") +","+ turtle.province.split(" ").join("+"))
-    }
-  }
   const findCoord = async (incData) => {
     return axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
       params:{
@@ -29,8 +23,17 @@ export default function Maps(props) {
       return res.data.results[0].geometry.location;
     }).catch((err) => console.log(err))
   }
+
+
   useEffect(() => {
     // Good!
+    const showAddress = (searchID) => {
+      const turtle = props.userList.find(user => user.id === searchID)
+      if (turtle) {
+        return (turtle.street.split(" ").join("+") +","+ turtle.city.split(" ").join("+") +","+ turtle.province.split(" ").join("+"))
+      }
+    }
+
     console.log("SETTER", props.setter)
     console.log("user", userID.user_id)
     Promise.all([
