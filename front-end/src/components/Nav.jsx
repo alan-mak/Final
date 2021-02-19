bd1bf0fe560d5c0bec48bc321cb5fee497458bc1
justinkwanchan <justinkwanchan@gmail.com>
2021-02-19 18:52:23 -0500

    Conditionally render logout button and clickability of logo - add background to logo

--- front-end/src/components/Nav.jsx
+++ front-end/src/components/Nav.jsx
@@ -3,21 +3,40 @@ import { Link } from 'react-router-dom';
 
 import './Nav.scss';
 
-export default function Nav(props) {
+export default function Nav() {
+  const token = sessionStorage.getItem('token');
+
+  const body = (
+    <div className='logo'>
+      <div>
+        <span className='logo-accent'>help</span>Me
+      </div>
+      <div>
+        help<span className='logo-accent'>You</span>
+      </div>
+    </div>
+  );
+
   return (
     <section id='nav-section'>
-      <Link to={'/choice'}>
-        <div className='logo'>
-          <div>
-            <span className='logo-red'>help</span>Me
-          </div>
-          <div>
-            help<span className='logo-red'>You</span>
-          </div>
-        </div>
-      </Link>
+      {token ? (
+        <Link to={'/choice'}>{body}</Link>
+      ) : (
+        <Link to={'#'} className='link-disabled'>
+          {body}
+        </Link>
+      )}
       <div className='nav-right'>
-        <Link to={'/'} onClick={() => {sessionStorage.removeItem("token")}}>Log out</Link>
+        {token && (
+          <Link
+            to={'/'}
+            onClick={() => {
+              sessionStorage.removeItem('token');
+            }}
+          >
+            Log out
+          </Link>
+        )}
       </div>
     </section>
   );
