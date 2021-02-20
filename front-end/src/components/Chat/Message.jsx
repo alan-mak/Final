f339d834fcf8dce1a5779cefb998c163efdefe3a
CD-Mackay <connor.mackay@gmail.com>
2021-02-20 14:44:53 -0500

    style messages component

--- front-end/src/components/Chat/Message.jsx
+++ front-end/src/components/Chat/Message.jsx
@@ -4,7 +4,7 @@ export class Message extends React.Component {
   render () {
     return (
       <div className="message-item">
-        <span>{this.props.text}</span>
+        <span>{this.props.senderName.slice(0, 3)} says ->: {this.props.text}</span>
       </div>
     )
   }
