16d8cb6be4c2cef06103a40e4a16ce021c57d052
CD-Mackay <connor.mackay@gmail.com>
2021-02-21 14:03:37 -0500

    implement filter function for accepted tasks

--- front-end/src/components/ShowPosted.jsx
+++ front-end/src/components/ShowPosted.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import Background from './Background';
+import jwt_decode from 'jwt-decode';
 
 export default function ShowPosted(props) {
+  const token = sessionStorage.getItem('token')
+  const userID = jwt_decode(token)
+
+  const acceptedTasks = props.state.tasks
+  .filter(task => task.helper_id === userID.user_id)
+  .map(task => <li key={task.id} id={task.id}>{task.name}</li>)
+  
   const body = (
-    <div>I am show posted</div>
+    <div>I am show posted
+      {acceptedTasks}
+    </div>
   )
   return (
     <Background body={body} />
