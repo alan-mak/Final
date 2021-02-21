435b1d6c0ed0be14ef17f4f1b712915fcbe5b932
justinkwanchan <justinkwanchan@gmail.com>
2021-02-21 18:49:51 -0500

    Add page styling and fix task data fetching

--- front-end/src/components/TaskItem/Show.jsx
+++ front-end/src/components/TaskItem/Show.jsx
@@ -1,28 +1,23 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Button from '../Button';
 
-import useApplicationData from '../../hooks/useApplicationData';
-
 import '../TaskItem/TaskItem.scss';
-import './Show.scss';
+import '../tasks.scss';
 
 export default function Show(props) {
-  const { getTask } = useApplicationData();
-
-  useEffect(() => {
-    getTask(props.match.params.task_id).then(res => {
-      console.log('***Task***:', res.data);
-    });
-  }, []);
+  const task = props.state.tasks.find(
+    task => task.id === parseInt(props.match.params.task_id)
+  );
 
   return (
     <section id='show-section'>
-      {console.log()}
-      <p>{props.description}</p>
-      <p>Estimated time: {props.time_to_complete}</p>
-      <div id='button-group'>
-        <Button id='task-accept' message='Accept!' />
-        <Button id='task-clarify' message='Clarify' />
+      <div className='task-item'>
+        <h2 className='task-list-item-title'>{task && task.name}</h2>
+        <div className='task-list-item-title-underline'></div>
+        <p className='task-list-item-description'>{task && task.description}</p>
+        <p className='task-list-item-duration'>
+          Estimated time: {task && task.duration}
+        </p>
       </div>
     </section>
   );
