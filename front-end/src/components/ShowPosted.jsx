fcd5ea8dc181b09f5cb832dca31f775a7bbe9168
CD-Mackay <connor.mackay@gmail.com>
2021-02-21 15:26:40 -0500

    style showposted page

--- front-end/src/components/ShowPosted.jsx
+++ front-end/src/components/ShowPosted.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 import Background from './Background';
 import jwt_decode from 'jwt-decode';
 import Button from './Button';
+import './ShowPosted.scss';
 export default function ShowPosted(props) {
   const token = sessionStorage.getItem('token')
   const userID = jwt_decode(token);
 
   const acceptedTasks = props.state.tasks
   .filter(task => task.helper_id === userID.user_id)
-  .map(task => <li key={task.id} id={task.id}>{task.name} <Button onClick={() => props.onComplete(task)} /></li>)
+  .map(task => <li className="posted-item" key={task.id} id={task.id}>{task.name} <Button message="mark completed!" onClick={() => props.onComplete(task)} /></li>)
   
   const body = (
-    <div>I am show posted
+    <div id="posted-container">
       {acceptedTasks}
     </div>
   )
