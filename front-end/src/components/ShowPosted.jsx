f5283648095bde684707f3abdb5225796f9700f5
CD-Mackay <connor.mackay@gmail.com>
2021-02-21 16:58:02 -0500

    implement posted tasks page

--- front-end/src/components/ShowPosted.jsx
+++ front-end/src/components/ShowPosted.jsx
@@ -14,14 +14,16 @@ export default function ShowPosted(props) {
 
   const acceptedTasks = props.state.tasks
   .filter(task => task.recipient_id === userID.user_id)
-  .map(task => <li className="posted-item" 
+  .map(task => <div className="posted-item" 
                    key={task.id} 
-                   id={task.id}>{task.name}
-                   {(task.helper_id && task.completed_at) && <p>Done! Thanks neighbor</p>}
+                   id={task.id}><h2 className="task-title" >{task.name}</h2>
+                   <div className="task-underline"></div>
+                   <p className="task-description" >{task.description}</p>
                    {(task.helper_id && !task.completed_at) && <Button message="mark completed!" onClick={() => completeTask(task)} />} 
-                   {!task.helper_id && <p>Nobody has accepted this task yet </p>}
+                   {!task.helper_id && <p className="task-message">Nobody has accepted this task yet </p>}
+                   {(task.helper_id && task.completed_at) && <p className="task-message">Done! Thanks neighbor</p>}
                    
-                   </li>)
+                   </div>)
   
   const body = (
     <div id="posted-container">
