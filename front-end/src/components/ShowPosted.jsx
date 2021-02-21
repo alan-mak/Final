7728f4a2c4b3bb2378d96e34f9d6097aa89a12f1
CD-Mackay <connor.mackay@gmail.com>
2021-02-21 15:38:49 -0500

    debug post request for accepted/completed at times

--- front-end/src/components/ShowPosted.jsx
+++ front-end/src/components/ShowPosted.jsx
@@ -4,11 +4,12 @@ import jwt_decode from 'jwt-decode';
 import Button from './Button';
 import './ShowPosted.scss';
 export default function ShowPosted(props) {
+  console.log(props);
   const token = sessionStorage.getItem('token')
   const userID = jwt_decode(token);
 
   const acceptedTasks = props.state.tasks
-  .filter(task => task.helper_id === userID.user_id)
+  .filter(task => task.recipient_id === userID.user_id)
   .map(task => <li className="posted-item" key={task.id} id={task.id}>{task.name} <Button message="mark completed!" onClick={() => props.onComplete(task)} /></li>)
   
   const body = (
