390d99d0aec933c67017f1e98663479f7fd2be5a
justinkwanchan <justinkwanchan@gmail.com>
2021-02-22 00:11:02 -0500

    Add validate function to Create component

--- front-end/src/components/TaskItem/Create.jsx
+++ front-end/src/components/TaskItem/Create.jsx
@@ -12,17 +12,39 @@ export default function Create(props) {
     title: '',
     description: '',
     duration: 0,
+    error: '',
   });
 
   const token = sessionStorage.getItem('token');
   const userID = jwt_decode(token);
 
   const history = useHistory();
+
   const autoFocus = useCallback(el => (el ? el.focus() : null), []);
+
+  const validate = () => {
+    setState(prev => ({ ...prev, error: '' }));
+    if (state.title && state.description && state.duration) {
+      props
+        .createTask(
+          state.title,
+          state.description,
+          state.duration,
+          userID.user_id
+        )
+        .then(res => {
+          props.setTasks(res.data);
+          history.push(`/tasks/${res.data.id}`);
+        });
+    } else {
+      setState(prev => ({ ...prev, error: 'Please fill in each field' }));
+    }
+  };
 
   const body = (
     <div id='create-container'>
       <h1>What would you like help with?</h1>
+      {state.error && <h2 id='create-error-message'>{state.error}</h2>}
       <form id='task-wrapper' onSubmit={event => event.preventDefault()}>
         <div className='form-group'>
           <label htmlFor='task-title'>Title of task</label>
@@ -33,7 +55,11 @@ export default function Create(props) {
             value={state.title}
             ref={autoFocus}
             onChange={event =>
-              setState(prev => ({ ...prev, title: event.target.value }))
+              setState(prev => ({
+                ...prev,
+                error: '',
+                title: event.target.value,
+              }))
             }
           />
         </div>
@@ -46,7 +72,11 @@ export default function Create(props) {
             value={state.description}
             name='description'
             onChange={event =>
-              setState(prev => ({ ...prev, description: event.target.value }))
+              setState(prev => ({
+                ...prev,
+                error: '',
+                description: event.target.value,
+              }))
             }
           />
         </div>
@@ -60,26 +90,18 @@ export default function Create(props) {
             name='time'
             // value={state.duration}
             onChange={event =>
-              setState(prev => ({ ...prev, duration: event.target.value }))
+              setState(prev => ({
+                ...prev,
+                error: '',
+                duration: event.target.value,
+              }))
             }
           />
         </div>
         <Button
           message='Post Task!'
           classes='create-button'
-          onClick={() =>
-            props
-              .createTask(
-                state.title,
-                state.description,
-                state.duration,
-                userID.user_id
-              )
-              .then(res => {
-                props.setTasks(res.data);
-                history.push(`/tasks/${res.data.id}`);
-              })
-          }
+          onClick={validate}
         />
       </form>
     </div>
