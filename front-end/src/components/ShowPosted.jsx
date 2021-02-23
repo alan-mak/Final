a05879776a8dc3d2309365e06223672597e1addb
CD-Mackay <connor.mackay@gmail.com>
2021-02-23 12:21:23 -0500

    style cancel button

--- front-end/src/components/ShowPosted.jsx
+++ front-end/src/components/ShowPosted.jsx
@@ -2,38 +2,26 @@ import React, { useState } from 'react';
 import Background from './Background';
 import jwt_decode from 'jwt-decode';
 import Button from './Button';
+import PostedItem from './PostedItem';
 import './ShowPosted.scss';
 export default function ShowPosted(props) {
   const [completed, setCompleted] = useState(false);
-  const [cancelled, setCancelled] = useState(false);
+  const [active, setActive] = useState(true);
   const token = sessionStorage.getItem('token')
   const userID = jwt_decode(token);
 
-  const completeTask = function (task) {
-    props.onComplete(task);
-    setCompleted(true);
-  }
-
-  const cancelTask = function (task) {
-    props.onCancel(task);
-    setCancelled(true);
-  }
-
   const acceptedTasks = props.state.tasks
   .filter(task => task.recipient_id === userID.user_id)
-  .map(task => <div className="posted-item" 
-                   key={task.id} 
-                   id={task.id}>
-                     <h2 className="task-title" >{task.name}</h2>
-                   <div className="task-underline"></div>
-                   <p className="task-description" >{task.description}</p>
-                   {(task.helper_id && !task.completed_at) && <Button message="Done?" onClick={() => completeTask(task)} /> 
-                   } 
-                   {!task.helper_id && <p className="task-message">Nobody has accepted this task yet </p>}
-                   {(task.helper_id && task.completed_at) && <p className="task-message" >Done! Thanks neighbor</p>}
-                   <Button onClick={() => cancelTask(task)} message="cancel" />
+  .map(task => <PostedItem
+                key={task.id} 
+                id={task.id}
+                name={task.name}
+                description={task.description}
+                complete={props.onComplete}
+                cancel={props.onCancel}
+                task={task} /> )
+                
                    
-                   </div>)
   
   const body = (
     <div id="posted-container">
