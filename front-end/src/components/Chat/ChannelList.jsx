da77515bc39d0786fc66eb9ae81a977f4403a79d
CD-Mackay <connor.mackay@gmail.com>
2021-03-04 11:51:15 -0500

    organize imports

--- front-end/src/components/Chat/ChannelList.jsx
+++ front-end/src/components/Chat/ChannelList.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-
 import { Channel } from './Channel';
+
 export class ChannelList extends React.Component {
   
   handleClick = id => {
     this.props.onselectchannel(id);
   }
+  
   render() {
     let list = `No chats currently available`;
     if (this.props.channels) {
